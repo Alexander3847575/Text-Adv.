cbf7581996f0c3695cc7d2557c3b3f832aa5c123
Alexander3847575 <51353507+Alexander3847575@users.noreply.github.com>
2019-06-04 15:56:51 -0700

    Create colorize.rb

---
+++ resources/colorize.rb
@@ -0,0 +1,99 @@
+# Credit to Erik Skoglund, BananaNeil, Ivan Black, and Juanito Fatas on stack overflow for the code (https://stackoverflow.com/questions/1489183/colorized-ruby-output)
+# Methods to stylize/colorize text
+# Usage: puts 'foo'.red (prints red text)
+# puts 'bar'.bg_black (prints text with a black background
+# puts 'foobar'.red.bg_black (prints red text with a black background)
+# puts 'blah'.bold.red.bg_black (priority)
+# puts "\e[31m" # set format (red foreground)
+# puts "\e[0m" (clear format)
+# puts "green-#{"red".red}-green".green  (will be green-red-normal, because of \e[0)
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def black
+    colorize(30)
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def brown
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def magenta
+    colorize(35)
+  end
+
+  def cyan
+    colorize(36)
+  end
+
+  def gray
+    colorize(37)
+  end
+  
+  def bg_black
+    colorize(40)
+  end
+
+  def bg_red
+    colorize(41)
+  end
+
+  def bg_green
+    colorize(42)
+  end
+
+  def bg_brown
+    colorize(43)
+  end
+
+  def bg_blue
+    colorize(44)
+  end
+
+  def bg_magenta
+    colorize(45)
+  end
+
+  def bg_cyan
+    colorize(46)
+  end
+
+  def bg_gray
+    colorize(47)
+  end
+
+  def bold
+    colorize(1)
+  end
+
+  def italic
+    colorize(3)
+  end
+
+  def underline
+    colorize(4)
+  end
+
+  def blink
+    colorize(5)
+  end
+
+  def reverse_color
+    colorize(7)
+  end
+end
